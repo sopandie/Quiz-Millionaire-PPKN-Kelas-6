036b28cf4664345b0e3ff115d973c632e57b2b38
sopandi <sopandi.salamalazhar@gmail.com>
2026-03-04 22:20:55 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,1102 @@
+const questions = [
+
+/* ================= LEVEL 1 ================= */
+/* NILAI-NILAI PANCASILA */
+
+{
+level: "LEVEL1",
+q: "Pancasila digunakan sebagai ... bangsa Indonesia.",
+options: [
+"Dasar negara",
+"Lagu kebangsaan",
+"Simbol daerah",
+"Peraturan sekolah"
+],
+correct: 0
+},
+
+{
+level: "LEVEL1",
+q: "Sila pertama Pancasila berbunyi ...",
+options: [
+"Persatuan Indonesia",
+"Ketuhanan Yang Maha Esa",
+"Kemanusiaan yang adil dan beradab",
+"Keadilan sosial bagi seluruh rakyat Indonesia"
+],
+correct: 1
+},
+
+{
+level: "LEVEL1",
+q: "Berdoa sebelum belajar merupakan contoh pengamalan sila ...",
+options: [
+"Persatuan Indonesia",
+"Kemanusiaan yang adil dan beradab",
+"Ketuhanan Yang Maha Esa",
+"Keadilan sosial"
+],
+correct: 2
+},
+
+{
+level: "LEVEL1",
+q: "Menghormati teman yang berbeda agama merupakan sikap ...",
+options: [
+"Egois",
+"Sombong",
+"Acuh",
+"Toleransi"
+],
+correct: 3
+},
+
+{
+level: "LEVEL1",
+q: "Lambang sila kedua Pancasila adalah ...",
+options: [
+"Rantai",
+"Bintang",
+"Pohon beringin",
+"Padi dan kapas"
+],
+correct: 0
+},
+
+{
+level: "LEVEL1",
+q: "Sila ketiga Pancasila berbunyi ...",
+options: [
+"Keadilan sosial",
+"Persatuan Indonesia",
+"Kemanusiaan yang adil dan beradab",
+"Ketuhanan Yang Maha Esa"
+],
+correct: 1
+},
+
+{
+level: "LEVEL1",
+q: "Gotong royong mencerminkan nilai ...",
+options: [
+"Keadilan sosial",
+"Kemanusiaan",
+"Persatuan",
+"Ketuhanan"
+],
+correct: 2
+},
+
+{
+level: "LEVEL1",
+q: "Lambang sila keempat Pancasila adalah ...",
+options: [
+"Pohon beringin",
+"Rantai",
+"Padi dan kapas",
+"Kepala banteng"
+],
+correct: 3
+},
+
+{
+level: "LEVEL1",
+q: "Sila kelima Pancasila berbunyi ...",
+options: [
+"Keadilan sosial bagi seluruh rakyat Indonesia",
+"Persatuan Indonesia",
+"Kemanusiaan yang adil dan beradab",
+"Ketuhanan Yang Maha Esa"
+],
+correct: 0
+},
+
+{
+level: "LEVEL1",
+q: "Menghargai sesama manusia merupakan pengamalan sila ...",
+options: [
+"Pertama",
+"Kedua",
+"Ketiga",
+"Keempat"
+],
+correct: 1
+},
+
+{
+level: "LEVEL1",
+q: "Cinta tanah air mencerminkan sila ...",
+options: [
+"Pertama",
+"Kedua",
+"Ketiga",
+"Kelima"
+],
+correct: 2
+},
+
+{
+level: "LEVEL1",
+q: "Tidak memaksakan kehendak merupakan sikap ...",
+options: [
+"Adil",
+"Bijaksana",
+"Demokratis",
+"Toleransi"
+],
+correct: 3
+},
+
+{
+level: "LEVEL1",
+q: "Pancasila dijadikan pedoman dalam ...",
+options: [
+"Kehidupan sehari-hari",
+"Permainan",
+"Olahraga",
+"Perjalanan"
+],
+correct: 0
+},
+
+{
+level: "LEVEL1",
+q: "Sikap menghormati guru merupakan pengamalan sila ...",
+options: [
+"Pertama",
+"Kedua",
+"Ketiga",
+"Kelima"
+],
+correct: 1
+},
+
+{
+level: "LEVEL1",
+q: "Menjaga persatuan di sekolah mencerminkan sila ...",
+options: [
+"Pertama",
+"Kedua",
+"Ketiga",
+"Keempat"
+],
+correct: 2
+},
+
+
+/* ================= LEVEL 2 ================= */
+/* PANCASILA DALAM KEHIDUPAN */
+
+{
+level: "LEVEL2",
+q: "Pancasila sebagai pandangan hidup berarti ...",
+options: [
+"Pedoman bertingkah laku",
+"Lambang negara",
+"Dasar wilayah",
+"Simbol daerah"
+],
+correct: 0
+},
+
+{
+level: "LEVEL2",
+q: "Kerja bakti membersihkan kelas merupakan contoh ...",
+options: [
+"Persaingan",
+"Kerja sama",
+"Permusuhan",
+"Pertengkaran"
+],
+correct: 1
+},
+
+{
+level: "LEVEL2",
+q: "Menghargai pendapat orang lain merupakan sikap ...",
+options: [
+"Egois",
+"Acuh",
+"Demokratis",
+"Sombong"
+],
+correct: 2
+},
+
+{
+level: "LEVEL2",
+q: "Membantu teman yang kesulitan belajar merupakan sikap ...",
+options: [
+"Malas",
+"Iri",
+"Sombong",
+"Peduli"
+],
+correct: 3
+},
+
+{
+level: "LEVEL2",
+q: "Mengutamakan kepentingan bersama adalah sikap ...",
+options: [
+"Gotong royong",
+"Egois",
+"Permusuhan",
+"Persaingan"
+],
+correct: 0
+},
+
+{
+level: "LEVEL2",
+q: "Menghormati guru merupakan sikap ...",
+options: [
+"Malas",
+"Beradab",
+"Sombong",
+"Egois"
+],
+correct: 1
+},
+
+{
+level: "LEVEL2",
+q: "Cinta produk dalam negeri menunjukkan sikap ...",
+options: [
+"Persaingan",
+"Toleransi",
+"Cinta tanah air",
+"Permusuhan"
+],
+correct: 2
+},
+
+{
+level: "LEVEL2",
+q: "Tidak membeda-bedakan teman merupakan sikap ...",
+options: [
+"Iri",
+"Egois",
+"Sombong",
+"Adil"
+],
+correct: 3
+},
+
+{
+level: "LEVEL2",
+q: "Menjaga kerukunan di sekolah mencerminkan ...",
+options: [
+"Persatuan",
+"Permusuhan",
+"Persaingan",
+"Perbedaan"
+],
+correct: 0
+},
+
+{
+level: "LEVEL2",
+q: "Musyawarah bertujuan mencapai ...",
+options: [
+"Kemenangan",
+"Kesepakatan",
+"Pertengkaran",
+"Persaingan"
+],
+correct: 1
+},
+
+{
+level: "LEVEL2",
+q: "Saling membantu mencerminkan sikap ...",
+options: [
+"Sombong",
+"Egois",
+"Peduli",
+"Iri"
+],
+correct: 2
+},
+
+{
+level: "LEVEL2",
+q: "Menolong korban bencana merupakan sikap ...",
+options: [
+"Acuh",
+"Egois",
+"Sombong",
+"Peduli"
+],
+correct: 3
+},
+
+{
+level: "LEVEL2",
+q: "Hidup rukun akan menciptakan ...",
+options: [
+"Kedamaian",
+"Pertengkaran",
+"Permusuhan",
+"Persaingan"
+],
+correct: 0
+},
+
+{
+level: "LEVEL2",
+q: "Menghargai orang lain menunjukkan sikap ...",
+options: [
+"Sombong",
+"Toleransi",
+"Egois",
+"Iri"
+],
+correct: 1
+},
+
+{
+level: "LEVEL2",
+q: "Menyelesaikan masalah dengan diskusi disebut ...",
+options: [
+"Bertengkar",
+"Berdebat",
+"Musyawarah",
+"Berperang"
+],
+correct: 2
+},
+
+/* ================= LEVEL 3 ================= */
+/* NORMA, HAK DAN KEWAJIBAN */
+
+{
+level: "LEVEL3",
+q: "Norma adalah aturan yang mengatur ...",
+options: [
+"Kehidupan masyarakat",
+"Permainan",
+"Olahraga",
+"Hiburan"
+],
+correct: 0
+},
+
+{
+level: "LEVEL3",
+q: "Norma agama berasal dari ...",
+options: [
+"Pemerintah",
+"Ajaran agama",
+"Sekolah",
+"Teman"
+],
+correct: 1
+},
+
+{
+level: "LEVEL3",
+q: "Menghormati orang tua merupakan contoh norma ...",
+options: [
+"Hukum",
+"Kesusilaan",
+"Kesopanan",
+"Agama"
+],
+correct: 2
+},
+
+{
+level: "LEVEL3",
+q: "Melanggar norma hukum akan mendapat ...",
+options: [
+"Hadiah",
+"Pujian",
+"Keuntungan",
+"Sanksi"
+],
+correct: 3
+},
+
+{
+level: "LEVEL3",
+q: "Hak adalah sesuatu yang ...",
+options: [
+"Layak diterima",
+"Harus dilakukan",
+"Dilarang",
+"Harus dihindari"
+],
+correct: 0
+},
+
+{
+level: "LEVEL3",
+q: "Kewajiban adalah sesuatu yang ...",
+options: [
+"Boleh dilakukan",
+"Harus dilakukan",
+"Tidak penting",
+"Harus dihindari"
+],
+correct: 1
+},
+
+{
+level: "LEVEL3",
+q: "Hak siswa di sekolah adalah ...",
+options: [
+"Menyapu kelas",
+"Mengerjakan tugas",
+"Mendapat pelajaran",
+"Membersihkan halaman"
+],
+correct: 2
+},
+
+{
+level: "LEVEL3",
+q: "Belajar dengan rajin merupakan ... siswa",
+options: [
+"Hak",
+"Hadiah",
+"Keuntungan",
+"Kewajiban"
+],
+correct: 3
+},
+
+{
+level: "LEVEL3",
+q: "Membantu orang tua di rumah merupakan ...",
+options: [
+"Kewajiban",
+"Hak",
+"Permainan",
+"Hiburan"
+],
+correct: 0
+},
+
+{
+level: "LEVEL3",
+q: "Hak dan kewajiban harus dilaksanakan secara ...",
+options: [
+"Tidak seimbang",
+"Seimbang",
+"Sembarang",
+"Tidak teratur"
+],
+correct: 1
+},
+
+{
+level: "LEVEL3",
+q: "Melaksanakan kewajiban menunjukkan sikap ...",
+options: [
+"Sombong",
+"Malas",
+"Tanggung jawab",
+"Egois"
+],
+correct: 2
+},
+
+{
+level: "LEVEL3",
+q: "Mendapat perlindungan adalah contoh ...",
+options: [
+"Kewajiban",
+"Larangan",
+"Peraturan",
+"Hak"
+],
+correct: 3
+},
+
+{
+level: "LEVEL3",
+q: "Mematuhi aturan sekolah merupakan ...",
+options: [
+"Kewajiban",
+"Hak",
+"Hadiah",
+"Permainan"
+],
+correct: 0
+},
+
+{
+level: "LEVEL3",
+q: "Contoh norma hukum adalah ...",
+options: [
+"Tidak sopan",
+"Mematuhi aturan lalu lintas",
+"Bermain",
+"Menonton TV"
+],
+correct: 1
+},
+
+{
+level: "LEVEL3",
+q: "Norma dibuat agar kehidupan menjadi ...",
+options: [
+"Kacau",
+"Sulit",
+"Tertib",
+"Bingung"
+],
+correct: 2
+},
+
+/* ================= LEVEL 4 ================= */
+/* MUSYAWARAH */
+
+{
+level: "LEVEL4",
+q: "Musyawarah adalah pengambilan keputusan secara ...",
+options: [
+"Bersama",
+"Sendiri",
+"Sepihak",
+"Rahasia"
+],
+correct: 0
+},
+
+{
+level: "LEVEL4",
+q: "Musyawarah bertujuan mencapai ...",
+options: [
+"Pertengkaran",
+"Kesepakatan",
+"Persaingan",
+"Kemenangan"
+],
+correct: 1
+},
+
+{
+level: "LEVEL4",
+q: "Dalam musyawarah setiap orang boleh ...",
+options: [
+"Berteriak",
+"Diam",
+"Menyampaikan pendapat",
+"Memaksa"
+],
+correct: 2
+},
+
+{
+level: "LEVEL4",
+q: "Keputusan musyawarah harus ...",
+options: [
+"Ditolak",
+"Dilupakan",
+"Ditinggalkan",
+"Dilaksanakan"
+],
+correct: 3
+},
+
+{
+level: "LEVEL4",
+q: "Contoh musyawarah di sekolah adalah ...",
+options: [
+"Memilih ketua kelas",
+"Bermain bola",
+"Belajar sendiri",
+"Menonton"
+],
+correct: 0
+},
+
+{
+level: "LEVEL4",
+q: "Sikap dalam musyawarah harus ...",
+options: [
+"Egois",
+"Menghargai pendapat orang lain",
+"Sombong",
+"Memaksa"
+],
+correct: 1
+},
+
+{
+level: "LEVEL4",
+q: "Musyawarah mencerminkan sila ...",
+options: [
+"Pertama",
+"Kedua",
+"Ketiga",
+"Keempat"
+],
+correct: 2
+},
+
+{
+level: "LEVEL4",
+q: "Tidak memaksakan pendapat menunjukkan sikap ...",
+options: [
+"Egois",
+"Sombong",
+"Malas",
+"Bijaksana"
+],
+correct: 3
+},
+
+{
+level: "LEVEL4",
+q: "Musyawarah menciptakan ...",
+options: [
+"Kebersamaan",
+"Permusuhan",
+"Persaingan",
+"Pertengkaran"
+],
+correct: 0
+},
+
+{
+level: "LEVEL4",
+q: "Keputusan musyawarah harus ...",
+options: [
+"Ditolak",
+"Dilaksanakan bersama",
+"Dilupakan",
+"Dihindari"
+],
+correct: 1
+},
+
+{
+level: "LEVEL4",
+q: "Setiap peserta musyawarah harus ...",
+options: [
+"Berteriak",
+"Memaksa",
+"Saling menghargai",
+"Diam"
+],
+correct: 2
+},
+
+{
+level: "LEVEL4",
+q: "Musyawarah menjaga ...",
+options: [
+"Permusuhan",
+"Persaingan",
+"Pertengkaran",
+"Persatuan"
+],
+correct: 3
+},
+
+{
+level: "LEVEL4",
+q: "Musyawarah mencerminkan sikap ...",
+options: [
+"Demokratis",
+"Egois",
+"Sombong",
+"Malas"
+],
+correct: 0
+},
+
+{
+level: "LEVEL4",
+q: "Tujuan musyawarah adalah ...",
+options: [
+"Berdebat",
+"Mencapai mufakat",
+"Bertengkar",
+"Menang sendiri"
+],
+correct: 1
+},
+
+{
+level: "LEVEL4",
+q: "Musyawarah harus dilakukan dengan ...",
+options: [
+"Marah",
+"Memaksa",
+"Bijaksana",
+"Terburu-buru"
+],
+correct: 2
+},
+
+/* ================= LEVEL 5 ================= */
+/* KEBERAGAMAN */
+
+{
+level: "LEVEL5",
+q: "Indonesia memiliki banyak suku dan budaya disebut ...",
+options: [
+"Keberagaman",
+"Keseragaman",
+"Persamaan",
+"Perbedaan"
+],
+correct: 0
+},
+
+{
+level: "LEVEL5",
+q: "Menghormati perbedaan agama disebut ...",
+options: [
+"Sombong",
+"Toleransi",
+"Egois",
+"Iri"
+],
+correct: 1
+},
+
+{
+level: "LEVEL5",
+q: "Semboyan negara Indonesia adalah ...",
+options: [
+"Indonesia Raya",
+"Garuda Pancasila",
+"Bhinneka Tunggal Ika",
+"Tut Wuri Handayani"
+],
+correct: 2
+},
+
+{
+level: "LEVEL5",
+q: "Bhinneka Tunggal Ika berarti ...",
+options: [
+"Satu bangsa",
+"Satu bahasa",
+"Satu budaya",
+"Berbeda-beda tetapi tetap satu"
+],
+correct: 3
+},
+
+{
+level: "LEVEL5",
+q: "Menghargai budaya daerah lain merupakan sikap ...",
+options: [
+"Toleransi",
+"Iri",
+"Sombong",
+"Egois"
+],
+correct: 0
+},
+
+{
+level: "LEVEL5",
+q: "Tidak mengejek teman yang berbeda suku merupakan ...",
+options: [
+"Permusuhan",
+"Toleransi",
+"Persaingan",
+"Kesombongan"
+],
+correct: 1
+},
+
+{
+level: "LEVEL5",
+q: "Keberagaman harus dijaga agar tercipta ...",
+options: [
+"Pertengkaran",
+"Persaingan",
+"Persatuan",
+"Perbedaan"
+],
+correct: 2
+},
+
+{
+level: "LEVEL5",
+q: "Jika tidak menghargai perbedaan dapat menyebabkan ...",
+options: [
+"Kerukunan",
+"Persatuan",
+"Kedamaian",
+"Perpecahan"
+],
+correct: 3
+},
+
+{
+level: "LEVEL5",
+q: "Menghargai budaya daerah lain menunjukkan sikap ...",
+options: [
+"Menghargai",
+"Sombong",
+"Iri",
+"Acuh"
+],
+correct: 0
+},
+
+{
+level: "LEVEL5",
+q: "Bekerja sama walau berbeda suku mencerminkan ...",
+options: [
+"Permusuhan",
+"Persatuan",
+"Persaingan",
+"Perbedaan"
+],
+correct: 1
+},
+
+{
+level: "LEVEL5",
+q: "Keberagaman budaya merupakan ... bangsa",
+options: [
+"Kelemahan",
+"Masalah",
+"Kekayaan",
+"Ancaman"
+],
+correct: 2
+},
+
+{
+level: "LEVEL5",
+q: "Tidak menghina agama lain merupakan sikap ...",
+options: [
+"Sombong",
+"Iri",
+"Egois",
+"Toleransi"
+],
+correct: 3
+},
+
+{
+level: "LEVEL5",
+q: "Saling menghargai perbedaan akan menciptakan ...",
+options: [
+"Kerukunan",
+"Permusuhan",
+"Persaingan",
+"Perpecahan"
+],
+correct: 0
+},
+
+{
+level: "LEVEL5",
+q: "Contoh sikap toleransi adalah ...",
+options: [
+"Mengejek teman",
+"Menghargai ibadah teman",
+"Tidak peduli",
+"Memaksa"
+],
+correct: 1
+},
+
+{
+level: "LEVEL5",
+q: "Keberagaman harus dijaga agar bangsa tetap ...",
+options: [
+"Bertengkar",
+"Berselisih",
+"Bersatu",
+"Terpecah"
+],
+correct: 2
+},
+
+/* ================= LEVEL 6 ================= */
+/* NKRI */
+
+{
+level: "LEVEL6",
+q: "Indonesia merupakan negara ...",
+options: [
+"Kesatuan",
+"Bagian",
+"Serikat",
+"Wilayah"
+],
+correct: 0
+},
+
+{
+level: "LEVEL6",
+q: "NKRI merupakan singkatan dari ...",
+options: [
+"Negara Kerajaan Republik Indonesia",
+"Negara Kesatuan Republik Indonesia",
+"Negara Kedaulatan Republik Indonesia",
+"Negara Kebangsaan Republik Indonesia"
+],
+correct: 1
+},
+
+{
+level: "LEVEL6",
+q: "Indonesia memiliki banyak provinsi sebagai ... negara",
+options: [
+"Musuh",
+"Saingan",
+"Bagian",
+"Ancaman"
+],
+correct: 2
+},
+
+{
+level: "LEVEL6",
+q: "Setiap warga negara harus menjaga ... bangsa",
+options: [
+"Perpecahan",
+"Perselisihan",
+"Pertengkaran",
+"Persatuan"
+],
+correct: 3
+},
+
+{
+level: "LEVEL6",
+q: "Cinta tanah air berarti ...",
+options: [
+"Bangga terhadap negara",
+"Tidak peduli negara",
+"Membenci negara",
+"Merusak negara"
+],
+correct: 0
+},
+
+{
+level: "LEVEL6",
+q: "Menggunakan produk dalam negeri merupakan sikap ...",
+options: [
+"Malas",
+"Cinta tanah air",
+"Acuh",
+"Sombong"
+],
+correct: 1
+},
+
+{
+level: "LEVEL6",
+q: "Menjaga persatuan merupakan pengamalan sila ...",
+options: [
+"Pertama",
+"Kedua",
+"Ketiga",
+"Keempat"
+],
+correct: 2
+},
+
+{
+level: "LEVEL6",
+q: "Menjaga kebersihan lingkungan merupakan sikap ...",
+options: [
+"Sombong",
+"Malas",
+"Acuh",
+"Tanggung jawab"
+],
+correct: 3
+},
+
+{
+level: "LEVEL6",
+q: "Setiap warga negara harus menjaga ... negara",
+options: [
+"Keutuhan",
+"Permusuhan",
+"Perselisihan",
+"Perpecahan"
+],
+correct: 0
+},
+
+{
+level: "LEVEL6",
+q: "Provinsi dipimpin oleh ...",
+options: [
+"Presiden",
+"Gubernur",
+"Camat",
+"Lurah"
+],
+correct: 1
+},
+
+{
+level: "LEVEL6",
+q: "NKRI harus dijaga oleh seluruh ...",
+options: [
+"Pejabat",
+"Tentara",
+"Warga negara",
+"Guru"
+],
+correct: 2
+},
+
+{
+level: "LEVEL6",
+q: "Jika persatuan tidak dijaga maka terjadi ...",
+options: [
+"Kerukunan",
+"Keamanan",
+"Kesejahteraan",
+"Perpecahan"
+],
+correct: 3
+},
+
+{
+level: "LEVEL6",
+q: "Menghormati simbol negara menunjukkan sikap ...",
+options: [
+"Nasionalisme",
+"Permusuhan",
+"Kesombongan",
+"Persaingan"
+],
+correct: 0
+},
+
+{
+level: "LEVEL6",
+q: "Contoh menjaga persatuan adalah ...",
+options: [
+"Bertengkar",
+"Kerja sama",
+"Memaksa",
+"Menghina"
+],
+correct: 1
+},
+
+{
+level: "LEVEL6",
+q: "Tujuan menjaga persatuan adalah agar negara ...",
+options: [
+"Bertengkar",
+"Lemah",
+"Kuat",
+"Terpecah"
+],
+correct: 2
+}
+
+];
